5668dbf55b0edbe61b710f0c58234cf1c0c45071
Jesse <jessedaniel@live.ca>
2021-07-19 12:26:44 -0600

    begin immutability practice

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
-import { compose, pipe } from "lodash/fp";
-
-let input = "  JavaScript  ";
-let output = "<div>" + input.trim() + "<div>";
-
-// trim string and wrap in div element
-
-const trim = (str) => str.trim();
-const wrap = (type) => (str) => `<${type}>${str}</${type}>`;
-const toLowerCase = (str) => str.toLowerCase();
-
-const transform = pipe(trim, toLowerCase, wrap("span"));
-console.log(transform(input));
+const person = { name: "John" };
