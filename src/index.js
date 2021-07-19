20a876c463f3d281798246fba353b1ec713287db
Jesse <jessedaniel@live.ca>
2021-07-19 11:47:07 -0600

    add wrap in span function

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ let output = "<div>" + input.trim() + "<div>";
 
 const trim = (str) => str.trim();
 const wrapInDiv = (str) => `<div>$${str}</div>`;
+const wrapInSpan = (str) => `<span>$${str}</span>`;
 const toLowerCase = (str) => str.toLowerCase;
 
 const transform = pipe(trim, toLowerCase, wrapInDiv);
