cb5fb26fc0e5c21b6d06eb524e5059d84be4ab38
Jesse <jessedaniel@live.ca>
2021-07-19 12:49:08 -0600

    use filter to remove

--- src/index.js
+++ src/index.js
@@ -4,4 +4,8 @@ const numbers = [1, 2, 3];
 
 const index = numbers.indexOf(2);
 const added = [...numbers.slice(0, index), 4, ...numbers.slice(index)];
-console.log(added);
+
+// removing
+const removed = numbers.filter((n) => n !== 2);
+
+console.log(removed);
