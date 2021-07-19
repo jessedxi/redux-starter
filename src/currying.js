3b1eb6bbcde0d554e86045d5b0803032a87d12c7
Jesse <jessedaniel@live.ca>
2021-07-19 12:02:13 -0600

    practice with arrow functions

--- src/currying.js
+++ src/currying.js
@@ -4,6 +4,8 @@ function add(a) {
   };
 }
 
-add(1)(5);
+const add2 = (a) => (b) => a + b;
+
+add(1)(5); //separate args using paranthesis
 
 // Currying takes a function with N args and reduces it to a function with 1 args
